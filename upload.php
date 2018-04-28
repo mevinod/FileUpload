604d8c7ceebaefe7ed6423a69f0a35631fa6db26
Vinod <connect@mevinod.com>
2018-04-28 12:38:28 +0530

    updated with bootsrtap for upload.php

--- upload.php
+++ upload.php
@@ -28,14 +28,33 @@ if(isset($name) &!empty($name)){
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Simple File Upload System in PHP & MySQL</title>
+	<title>Simple File Upload</title>
 	<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
 <!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
+
+	<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">
+        <img alt="F" src="filetype-docs-icon (1).png">
+      </a>
+    </div>
+  </div>
+</nav>
+
+<div class="page-header">
+  <h1>Simple File Upload Systm</h1>
+</div>
+
 <div class="container">
 	<div class="row">
 		<form class="col-md-6 col-md-offset-3" method="post" enctype="multipart/form-data">
